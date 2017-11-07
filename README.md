0eea5165ae99480f2ae4fb5fd850148fbfe4b9df
Frank Denis <github@pureftpd.org>
2017-11-07 17:39:09 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,44 @@
+Jumpin' Jack
+============
+
+Jumpin' Jack is a trivial and ugly, but still maybe potentially useful script to uncover conditional jumps added by smart compilers.
+
+This can be useful if the original intent was to build constant-time code.
+
+Usage
+-----
+
+* Disssemble the compiled library to analyze using binutils'`objdump` (`gobjdump` on MacOS):
+
+```sh
+objdump -d -l /usr/local/lib/libfoo.so > foo.odump
+```
+
+* Create annotated shadow copies of the source code:
+
+```sh
+python jumpinjack.py -b /path/to/the/source/code -d foo.odump
+```
+
+Annotated copies have a `.tagged` suffix. Review these by grepping for `JJ: JUMP` trailing comments. All of these indicate conditional jumps inserted by the compiler:
+
+```c
+i = len;
+while (i != 0U) {   /*** JJ: JUMP (jne 21368 <sodium_compare+0x25>) ***/
+    i--;
+    x1 = b1[i];
+    x2 = b2[i];
+    gt |= ((x2 - x1) >> 8) & eq;
+    eq &= ((x2 ^ x1) - 1) >> 8;
+}
+return (int) (gt + gt + eq) - 1;
+```
+
+Conditional jumps right before or after an actual control flow statement are expected. However, you may also stumble upon lines annoted with `JJ: JUMP!` (note the exclamation mark):
+
+```c
+a  = f[0] * sn;
+h0 = ((uint64_t) a) & mask; /*** JJ: JUMP! (jne 1d4c1 <crypto_scalarmult_curve25519_ref10+0x19f>) ***/
+```
+
+You should carefully review these first and they are more likely to contain conditional jumps inserted by the compiler, that are not explicitly visible in the source code.
---
+++ jumpinjack.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python
+
+import getopt
+import sys
+import re
+
+file, line_nb = None, 0
+
+affected_files = {}
+
+
+def tag_file(affected_file, affected_lines):
+    with open(affected_file) as f:
+        tagged_file = affected_file + ".tagged"
+        with open(tagged_file, "w") as h:
+            line_nb = 0
+            for line in f:
+                line_nb = line_nb + 1
+                if line_nb in affected_lines:
+                    detail = affected_lines[line_nb]
+                    if re.match(".*(for|while|do|if|else|assert|break|switch|return|\}).*", line):
+                        line = line.rstrip() + "\t/*** JJ: JUMP (" + detail + ") ***/\n"
+                    else:
+                        line = line.rstrip() + "\t/*** JJ: JUMP! (" + detail + ") ***/\n"
+                h.write(line)
+            print(tagged_file)
+
+
+def tag(base_dir, dump_file):
+    file, line_nb = None, 0
+    for line in open(dump_file):
+        if re.match("^/.+[.].+:[0-9]+$", line):
+            file_, line_nb_ = line.strip().split(':')
+            if file_.startswith(base_dir):
+                file, line_nb = file_, int(line_nb_)
+            else:
+                file, line_nb = None, 0
+            continue
+
+        if not file:
+            continue
+
+        parts = line.strip().split("\t")
+        if len(parts) < 3:
+            continue
+
+        opcode = parts[2].split(' ')[0]
+
+        if not re.match("^(ja|jae|jb|jbe|jc|jcxz|je|jecxz|jg|jge|jl|jle|jna|jnae|" +
+                        "jnb|jnbe|jnc|jne|jng|jnge|jnl|jnle|jno|jnp|jns|jnz|jo|" +
+                        "jp|jpe|jpo|js|jz)$", opcode):
+            continue
+
+        if file not in affected_files:
+            affected_files[file] = {}
+
+        affected_files[file][line_nb] = re.sub("\s+", " ", parts[2])
+
+    for affected_file in affected_files:
+        try:
+            tag_file(affected_file, affected_files[affected_file])
+        except IOError:
+            pass
+
+
+def usage():
+    print("Usage: jumpinjack.py -b <base dir> -d <objdump file>")
+    sys.exit(2)
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "b:d:")
+except getopt.GetoptError as err:
+    usage()
+
+base_dir, dump_file = None, None
+
+for o, a in opts:
+    if o == "-b":
+        base_dir = a
+    elif o == "-d":
+        dump_file = a
+
+if (not base_dir) or (not dump_file):
+    usage()
+
+tag(base_dir, dump_file)
